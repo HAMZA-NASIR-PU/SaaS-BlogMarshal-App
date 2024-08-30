0f7bf5ddcb0f7014575f55c24298f2886409233b
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-08-30 12:18:26 +0500

    Add shadeCn UI Library

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>HELLO WORLD</h1>
+      <Button>HELLO WORLD</Button>
+    </div>
+  );
+}
